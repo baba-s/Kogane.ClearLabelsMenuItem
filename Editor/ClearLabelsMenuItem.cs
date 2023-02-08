f3dfbc6d0eb95bbe5365584e384748d5a3617c5d
baba-s <shotababa1206@gmail.com>
2023-02-08 19:35:25 +0900

    add

---
+++ Editor/ClearLabelsMenuItem.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+    internal static class ClearLabelsMenuItem
+    {
+        private const string MENU_ITEM_NAME = @"Assets/Kogane/Clear Labels";
+
+        [MenuItem( MENU_ITEM_NAME, true )]
+        private static bool CanCopy()
+        {
+            return Selection.assetGUIDs is { Length: > 0 };
+        }
+
+        [MenuItem( MENU_ITEM_NAME, false, 1142154112 )]
+        private static void Copy()
+        {
+            var assetGUIDs = Selection.assetGUIDs;
+
+            if ( assetGUIDs is not { Length: > 0 } ) return;
+
+            try
+            {
+                AssetDatabase.StartAssetEditing();
+
+                var assets = assetGUIDs
+                        .Select( x => AssetDatabase.GUIDToAssetPath( x ) )
+                        .Select( x => AssetDatabase.LoadAssetAtPath<Object>( x ) )
+                        .ToArray()
+                    ;
+
+                foreach ( var asset in assets )
+                {
+                    AssetDatabase.ClearLabels( asset );
+                }
+            }
+            finally
+            {
+                AssetDatabase.StopAssetEditing();
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.ClearLabelsMenuItem
+# Kogane Clear Labels Menu Item
+
+アセットに設定されているラベルをすべて解除する MenuItem を追加するエディタ拡張
